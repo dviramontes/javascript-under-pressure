b2fb997eb17f3dadb709f6d34a8b77c2a5bc3e85
david viramontes <dviramontes@gmail.com>
2013-11-01 09:55:07 -0600

    combines lvl1 and 2

---
+++ level1-and-2.js
@@ -0,0 +1,9 @@
+// level 1
+function test1(i){
+	return i * 2;
+}
+// level 2
+
+function test2(i){
+	return i % 2 ?  false : true;
+}
--- level2.js
+++
@@ -1,5 +0,0 @@
-// level 2
-
-function test(x){
-	return x % 2 ?  false : true;
-}
